7c6b06b7f111a0ed3ffc2c981c02d1da76af2b80
wes <trinhpt@uci.edu>
2021-08-16 09:20:55 -0700

    added more users

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -9,12 +9,26 @@ def seed_users():
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
+    nico = User(
+        username='nico', email='nico@gmail.com', password='password')
+    leslie = User(
+        username='leslie', email='leslie@gmail.com', password='password')
+    casey = User(
+        username='casey', email='casey@gmail.com', password='password')
+    wes = User(
+        username='wes', email='wes@gmail.com', password='password')
+
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(nico)
+    db.session.add(leslie)
+    db.session.add(casey)
+    db.session.add(wes)
 
     db.session.commit()
+
 
 
 # Uses a raw SQL query to TRUNCATE the users table.
