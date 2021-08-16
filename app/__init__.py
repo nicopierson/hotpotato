ce64725e7c173482e7acbd9e3782c0037ea155d2
wes <trinhpt@uci.edu>
2021-08-16 14:47:53 -0700

    inital routes recipes

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.recipes import recipe_routes
 
 from .seeds import seed_commands
 
@@ -31,6 +32,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(recipe_routes, url_prefix='/api/recipes')
+
 db.init_app(app)
 Migrate(app, db)
 
