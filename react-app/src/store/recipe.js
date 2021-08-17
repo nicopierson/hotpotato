5451246712a5ea38efba40ec54b50070ce4154df
Nico Pierson <nicogpt@gmail.com>
2021-08-17 11:16:29 -0700

    finished create recipe redux

--- react-app/src/store/recipe.js
+++ react-app/src/store/recipe.js
@@ -1,6 +1,7 @@
 const SET_RECIPE = 'recipes/setRecipe';
 const SET_ALL_RECIPES = 'recipes/setAllRecipes';
 const DELETE_RECIPE = 'recipes/deleteRecipe';
+const ADD_RECIPE = 'recipes/addRecipe';
 
 const setRecipe = (recipe) => ({
     type: SET_RECIPE,
@@ -15,6 +16,11 @@ const setAllRecipes = (recipes) => ({
 const deleteARecipe = (id) => ({
     type: DELETE_RECIPE,
     recipeId: id,
+});
+
+const addRecipe = (recipe) => ({
+    type: ADD_RECIPE,
+    recipe,
 });
 
 export const getRecipe = (id) => async (dispatch) => {
@@ -41,6 +47,7 @@ export const getAllRecipes = () => async (dispatch) => {
     }
 }
 
+// TODO Test State
 export const deleteRecipe = (id) => async (dispatch) => {
     const response = await fetch(`/api/recipes/${id}`, {
         method: 'DELETE',
@@ -54,6 +61,23 @@ export const deleteRecipe = (id) => async (dispatch) => {
     }
 }
 
+// TODO Test State
+export const createRecipe = (payload) => async (dispatch) => {
+    const response = await fetch(`/api/recipes`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ payload }),
+    });
+
+    if (response.ok) {
+        const recipe = await response.json();
+        await dispatch(addRecipe(recipe));
+        return recipe;
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+}
+
 export default function reducer(state = {}, action) {
     let newState = {}
     switch (action.type) {
@@ -62,11 +86,16 @@ export default function reducer(state = {}, action) {
             newState[action.recipe.id] = action.recipe;
             return newState;
         case SET_ALL_RECIPES:
-            newState = {}
             action.recipes.recipes.forEach(recipe => {
                 newState[recipe.id] = recipe;
             });
             return { ...state, ...newState };
+        case ADD_RECIPE:
+            newState = { ...state };
+            if (!state[action.recipe.id]) {
+                newState = { ...state, [action.recipe.id]: action.recipe };
+            }
+            return newState;
         case DELETE_RECIPE:
             newState = { ...state };
             delete newState[action.recipeId];
