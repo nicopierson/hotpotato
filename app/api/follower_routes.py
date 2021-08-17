891f5e2a2c4ca07c4e20136277755a8811566c84
Nico Pierson <nicogpt@gmail.com>
2021-08-16 20:03:42 -0700

    added documentation and add_follower route

--- app/api/follower_routes.py
+++ app/api/follower_routes.py
@@ -1,22 +1,40 @@
-from flask import Blueprint, json, jsonify
+from flask import Blueprint, jsonify
 from app.models import db, User
+from flask_login import current_user
 
 follower_routes = Blueprint('followers', __name__)
 
 
 @follower_routes.route('/')
 def followers():
+    """
+    Retrieves all followers and returns followed and followers dict
+    """
     followers = User.get_all_followers()
-    return jsonify([ {"follow_owner": followed.to_dict(), "follower": follower.to_dict()} for followed, follower in followers])
+    return jsonify([ {"followed": followed.to_dict(), "follower": follower.to_dict()} for followed, follower in followers])
 
 
-@follower_routes.route('/<int:id>')
+@follower_routes.route('/users/<int:id>')
 def get_followers(id):
+    """
+    Retrieves all followers for a user
+    """
     followers = User.query.get(id).get_followers()
     return jsonify([ user.to_dict() for user in followers ])
     
 
-@follower_routes.route('/')
+@follower_routes.route('/users/<int:id>', methods=['POST'])
+def add_follower(id):
+    """
+    Follow a user
+    """
+    follower = User.query.get(current_user.id)
+    followed = User.query.get(id)
+    if follower:
+        follower.follow(followed)
+        db.session.commit()
+    else:
+        return {'errors': ['Not Found']}, 404
 # for post
     # user = User.query.get(2)
     # user7 = User.query.get(7)
