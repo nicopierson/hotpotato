af0c799df27803c068f15def3d72d588be9df779
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-08-17 12:00:19 -0400

    like routes

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(like_routes, url_prefix='api/likes')
+app.register_blueprint(like_routes, url_prefix='/api/likes')
 db.init_app(app)
 Migrate(app, db)
 
