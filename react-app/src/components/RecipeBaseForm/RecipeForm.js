ffefba923f90d1c8e82ae6c27de4d1c4e345ce6a
wes <trinhpt@uci.edu>
2021-08-18 14:18:01 -0700

    recipeForm adding form logic

--- react-app/src/components/RecipeBaseForm/RecipeForm.js
+++ react-app/src/components/RecipeBaseForm/RecipeForm.js
@@ -5,6 +5,32 @@ import { createRecipe } from '../../store/recipe';
 import { useHistory } from 'react-router';
 
 export const RecipeForm = () => {
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const sessionUser = useSelector((state) => state.session.user);
+
+  // const onFormSubmitCreateRecipeBase = (e)=>{
+  //   e.preventDefault();
+  //   if(!sessionUser) history.push('/')
+  //   else{
+  //     const payload = {
+
+  //       // payload from state
+  //     }
+  //     setErrors([]);
+  //     return dispatch(createItemThunk(payload,listId)).then((data)=>{
+  //       // console.log("item creation returned data: ", data);
+  //       history.push(`/view-list/${listId}`);
+  //       window.location.reload();
+  //     }).catch(async (res) =>{
+  //       const data = await res.json();
+  //       if(data && data.errors) setErrors(data.errors);
+  //     })
+  //   }
+  // }
+
+
   return (
     <div>
       Hello from recipe form
