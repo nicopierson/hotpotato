009014c391023c0407154414abe63cd8d8901250
Nico Pierson <nicogpt@gmail.com>
2021-08-18 10:42:21 -0700

    formatted contributors and social links

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -8,54 +8,62 @@ const Footer = () => {
         <div className={styles.footer_social}>
           <div className={styles.footer_casey}>
             <p>Casey Tuer</p>
-            <a href='https://github.com/caseytuer'>
-              <i className='fab fa-github'></i>
-            </a>
-            <a href='https://www.linkedin.com/in/caseytuer/'>
-              <i className='fab fa-linkedin'></i>
-            </a>
-            <a href='#'>
-              <i className='fab fa-angellist'></i>
-            </a>
+            <div>
+              <a href='https://github.com/caseytuer'>
+                <i className='fab fa-github'></i>
+              </a>
+              <a href='https://www.linkedin.com/in/caseytuer/'>
+                <i className='fab fa-linkedin'></i>
+              </a>
+              <a href='#'>
+                <i className='fab fa-angellist'></i>
+              </a>
+            </div>
           </div>
           <div className={styles.footer_nico}>
             <p>Leslie Owiti</p>
-            <a href='https://github.com/leslieowititech'>
-              <i className='fab fa-github'></i>
-            </a>
-            <a href='https://www.linkedin.com/in/leslie-owiti-0b447952/'>
-              <i className='fab fa-linkedin'></i>
-            </a>
-            <a href='#'>
-              <i className='fab fa-angellist'></i>
-            </a>
+            <div>
+              <a href='https://github.com/leslieowititech'>
+                <i className='fab fa-github'></i>
+              </a>
+              <a href='https://www.linkedin.com/in/leslie-owiti-0b447952/'>
+                <i className='fab fa-linkedin'></i>
+              </a>
+              <a href='#'>
+                <i className='fab fa-angellist'></i>
+              </a>
+            </div>
           </div>
           <div className={styles.footer_nico}>
             <p>Nico Pierson</p>
-            <a href='https://github.com/nicopierson'>
-              <i className='fab fa-github'></i>
-            </a>
-            <a href='https://www.linkedin.com/in/nico-pierson/'>
-              <i className='fab fa-linkedin'></i>
-            </a>
-            <a href='https://angel.co/u/nico-gerard-pierson'>
-              <i className='fab fa-angellist'></i>
-            </a>
+            <div>
+              <a href='https://github.com/nicopierson'>
+                <i className='fab fa-github'></i>
+              </a>
+              <a href='https://www.linkedin.com/in/nico-pierson/'>
+                <i className='fab fa-linkedin'></i>
+              </a>
+              <a href='https://angel.co/u/nico-gerard-pierson'>
+                <i className='fab fa-angellist'></i>
+              </a>
+            </div>
           </div>
           <div className={styles.footer_nico}>
             <p>Wes Trinh</p>
-            <a href='https://github.com/WesTrinhKL'>
-              <i className='fab fa-github'></i>
-            </a>
-            <a href='#'>
-              <i className='fab fa-linkedin'></i>
-            </a>
-            <a href='#'>
-              <i className='fab fa-angellist'></i>
-            </a>
+            <div>
+              <a href='https://github.com/WesTrinhKL'>
+                <i className='fab fa-github'></i>
+              </a>
+              <a href='#'>
+                <i className='fab fa-linkedin'></i>
+              </a>
+              <a href='#'>
+                <i className='fab fa-angellist'></i>
+              </a>
+            </div>
           </div>
         </div>
-        <p>Copyright © 2021 Hotpotato Inc. All rights reserved.</p>
+        <p className={styles.footer_copyright}>Copyright © 2021 Hotpotato Inc. All rights reserved.</p>
       </div>
   );
 };
