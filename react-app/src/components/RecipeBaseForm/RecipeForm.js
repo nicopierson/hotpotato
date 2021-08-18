022e7b33c63b2f5cdcd76d9480bd83fdc2a813cd
wes <trinhpt@uci.edu>
2021-08-18 15:27:31 -0700

    recipe base create form success

--- react-app/src/components/RecipeBaseForm/RecipeForm.js
+++ react-app/src/components/RecipeBaseForm/RecipeForm.js
@@ -1,4 +1,4 @@
-import React , { useState }from 'react';
+import React , { useState, useEffect}from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './CreateRecipeForm.css';
 import { createRecipe } from '../../store/recipe';
@@ -8,32 +8,90 @@ export const RecipeForm = () => {
 
   const dispatch = useDispatch();
   const history = useHistory();
-  const sessionUser = useSelector((state) => state.session.user);
+  const user = useSelector((state) => state.session.user);
 
-  // const onFormSubmitCreateRecipeBase = (e)=>{
-  //   e.preventDefault();
-  //   if(!sessionUser) history.push('/')
-  //   else{
-  //     const payload = {
 
-  //       // payload from state
-  //     }
-  //     setErrors([]);
-  //     return dispatch(createItemThunk(payload,listId)).then((data)=>{
-  //       // console.log("item creation returned data: ", data);
-  //       history.push(`/view-list/${listId}`);
-  //       window.location.reload();
-  //     }).catch(async (res) =>{
-  //       const data = await res.json();
-  //       if(data && data.errors) setErrors(data.errors);
-  //     })
-  //   }
-  // }
+  const [title, setTitle] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  // const [userId, setUserId] = useState(1);
+  const [errors, setErrors] = useState([]);
+
+  let userId;
+  if(user) userId = user.id;
+  console.log("this is the user id from recipeForm!!", user.id)
+
+
+  const setTitleE = (e) => setTitle(e.target.value);
+  const setImageUrlE = (e) => setImageUrl(e.target.value);
+
+  const onFormSubmitCreateRecipeBase = (e)=>{
+    e.preventDefault();
+    if(!user) history.push('/')
+    else{
+      const payload = {
+
+        title,
+        imageUrl,
+        userId
+      }
+      setErrors([]);
+      return dispatch(createRecipe(payload)).then((data)=>{
+        //add push to view-recipe/id later
+        history.push(`/`);
+        window.location.reload();
+      }).catch(async (res) =>{
+        const data = await res.json();
+        if(data && data.errors) setErrors(data.errors);
+      })
+    }
+  }
 
 
   return (
     <div>
       Hello from recipe form
+      <form onSubmit={onFormSubmitCreateRecipeBase}>
+
+        <div className=""> Start Your Creation</div>
+          <ul className="error-group">
+              {errors.map((error, idx) => <li key={idx}>*{error}</li>)}
+          </ul>
+
+          {/* FORM INPUTS */}
+          <div className="">
+            <label className="">
+              Title:
+            </label>
+            <input className=""
+                placeholder="title your awesome creation here"
+                required
+                value={title}
+                onChange={setTitleE}
+                type="text" />
+          </div>
+
+          <div className="input-wrapper">
+            <label className="form-label-spacing">
+              iage url (optional)
+            </label>
+
+            <input className="input-box-style"
+                placeholder="www.mybestrecipeimage.com"
+                required
+                value={imageUrl}
+                onChange={setImageUrlE}
+                type="text" />
+            <div className="cover-photo-preview">
+              <img src={imageUrl} alt="Can't Find" />
+            </div>
+          </div>
+
+          <div className="input-wrapper">
+            <button className="submit-list-button" type="submit">Start Creation</button>
+          </div>
+
+
+      </form>
     </div>
   )
 }
