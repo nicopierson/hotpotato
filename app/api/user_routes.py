d4427bfa840f761c7ec838f7f34d157b3fe14fec
Nico Pierson <nicogpt@gmail.com>
2021-08-19 11:08:31 -0700

    added route to update user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
+from app.forms.profile_form import ProfileForm
+from .recipes import current_user_matches_client_user, authorization_errors_to_error_messages
 
 
 user_routes = Blueprint('users', __name__)
@@ -19,7 +21,16 @@ def user(id):
     return user.to_dict()
 
 
+# TODO test the editing of this route, password is not added to ProfileForm: keep old pw
 @user_routes.route('/<int:id>', methods=['PUT'])
-def user(id):
-    user = User.query.get(id)
-    return user.to_dict()
+def profile_update(id):
+        form = ProfileForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            if current_user_matches_client_user(form.user_id.data):
+                user = User.query.get(id)
+                form.populate_obj(user)
+                db.session.add(user)
+                db.session.commit()
+                return user.to_dict()
+            return authorization_errors_to_error_messages("Please try to post as yourself! Unauthorized Access.")
