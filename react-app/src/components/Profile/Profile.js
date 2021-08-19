43443f84e1733f7ee06ca6e13c95da52a5cef4ad
Nico Pierson <nicogpt@gmail.com>
2021-08-19 13:56:35 -0700

    added profile to redux store and output profile in component

--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -1,27 +1,35 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getProfile } from '../../store/profile';
 
 import ProfileCard from './ProfileCard';
 
 const Profile = () => {
     const { userId } = useParams();
+
+    /* isOwner used to check if owner of profile page */
     const user = useSelector(state => state.session.user);
     const isOwner = user.id === +userId;
+
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.profile[userId]);
+
+    useEffect(() => {
+        dispatch(getProfile(userId))
+    }, [dispatch, userId]);
 
     return (
         <div>
             <br/>
             <br/>
             <br/>
-            <h2>Profile</h2>
-            <p>User {userId} </p>
-            {isOwner &&
-                <h3>I am the owner of this page</h3>
+            {profile &&
+                <ProfileCard 
+                    profile={profile}
+                />
             }
-            <ProfileCard 
-                userId={userId}
-            />
         </div>
     )
 };
--- react-app/src/components/Profile/ProfileCard.js
+++ react-app/src/components/Profile/ProfileCard.js
@@ -1,40 +1,57 @@
-import { useState } from 'react';
-
 import styles from './ProfileCard.module.css';
 import profileImg from '../../assets/images/demo_profile.png';
 
-const ProfileCard = ({ userId }) => {
+const ProfileCard = ({ profile }) => {
 
-    
+
 
     return (
         <div className={styles.inner_container}>
-            <h2>Profile Card</h2>
             <div className={styles.profile_img}>
                 <img alt='profile' src={profileImg} />
             </div>
             <div className={styles.profile_username}>
-
+                <i className='far fa-user'></i>
+                { profile.username }
             </div>
             <div className={styles.profile_email}>
-
-            </div>
-            <div className={styles.profile_follow_button}>
-
-            </div>
-            <div className={styles.profile_appreciations}>
-
-            </div>
-            <div className={styles.profile_followers}>
-
+                <i className='far fa-envelope'></i>
+                { profile.email }
             </div>
             <div className={styles.profile_following}>
-
+                
             </div>
-            <div className={styles.profile_about}>
+            <div className={styles.profile_following_button}>
+                
+            </div>
+            <div className={styles.profile_appreciations}>
+                <div>
+                    Appreciations
+                </div>
+                <div>
+                    { profile.appreciations }
+                </div>
+            </div>
+            <div className={styles.profile_followers}>
+                <div>
+                    Followers
+                </div>
+                <div>
+                    { profile.followers }
+                </div>
+            </div>
+            <div className={styles.profile_following}>
+                <div>
+                    Followings
+                </div>
+                <div>
+                    { profile.followings }
+                </div>
+            </div>
+            {/* <div className={styles.profile_about}>
                 <h3>About Me</h3>
                 <p>Lorum ipsum</p>
-            </div>
+            </div> */}
         </div>
     )
 };
