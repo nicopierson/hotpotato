da47babe709101bb9affe15223c853957883f7b0
Casey Tuer <caseytuer@gmail.com>
2021-08-18 22:33:34 -0700

    demo button functioning

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -1,11 +1,28 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
-import './NavBar.css'
 import { CreateRecipeButton } from './CreateRecipeButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../store/session';
+import './NavBar.css'
 
 const NavBar = () => {
+
+  const [errors, setErrors] = useState([]);
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+
+ 
+
+  const demoLogin = async () => {
+    const data = await dispatch(login('demo@aa.io', 'password'));
+    if (data) {
+      setErrors(data);
+    }
+  }
+
   return (
     <nav className="navbar-container">
       <ul className="navbar-ul">
@@ -27,30 +44,37 @@ const NavBar = () => {
           />
         </div>
         <div className="navbar-li navbar-create-and-auth">
-          <li>
-            <button>Create</button>
-          </li>
-          <li>
-            <NavLink to='/login' exact={true} activeClassName='active'>
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/sign-up' exact={true} activeClassName='active'>
-              Sign Up
-            </NavLink>
-          </li>
-          {/* <li>
-            <NavLink to='/users' exact={true} activeClassName='active'>
-              Users
-            </NavLink>
-          </li> */}
-          <li className="button-fix">
-            <CreateRecipeButton />
-          </li>
-          <li className="navbar-li">
-            <LogoutButton />
-          </li>
+          
+          {!user &&
+          <>
+            <li>
+              <NavLink to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </li>
+          </>
+          }
+          {!user && 
+          <button
+            id="demo-login-btn"
+            onClick={demoLogin}
+          >Demo Login</button>
+          }
+          {user && 
+          <>
+            <li className="button-fix">
+              <CreateRecipeButton />
+            </li>
+            <li className="navbar-li">
+              <LogoutButton />
+            </li>
+          </>
+          }
         </div>
       </ul>
     </nav>
