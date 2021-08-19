855faadfc8ed1b76a02e82259a9f7af409c1b685
wes <trinhpt@uci.edu>
2021-08-18 21:27:18 -0700

    added route and page for recipe details

---
+++ react-app/src/components/Recipe/RecipeBaseDetails.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router';
+// import { useDispatch } from 'react-redux';
+
+import { useSelector } from 'react-redux';
+
+const RecipeBaseDetails = () => {
+  let {id} = useParams();
+  // const dispatch = useDispatch()
+  const sessionUser = useSelector((state) => state.session.user);
+  const recipeDetails= useSelector((state) => state.recipe);
+
+
+  const [name, setName] = useState('');
+  const [thumbnail_url, setThumbnail_url] = useState('');
+  const [likes, setLikes] = useState(null);
+  const [recipeId, setRecipeId] = useState(null);
+
+  if (recipeDetails !== undefined) {
+    const recipeBaseDetails = recipeDetails['`${id}`']
+    console.log('recipe details', recipeBaseDetails);
+    // for (const [key,value] of Object.entries(recipeBaseDetails)){
+    //   console.log(key, value)
+    // }
+
+    // setName(recipeBaseDetails['name'])
+    // setThumbnail_url(recipeBaseDetails['thumbnail_url'])
+    // setLikes(recipeBaseDetails.likes)
+    // setRecipeId(recipeBaseDetails.id)
+  }
+  if(recipeDetails !== undefined && recipeDetails !== null ) console.log('yes', recipeDetails['1'])
+
+
+  // useEffect(()=>{
+  //   dispatch(getRecipe(id))
+  // }, [dispatch, id])
+
+
+
+  return (
+    <div>
+      <div>
+        name: {name}
+      </div>
+      <div>
+        thumbnail_url: {thumbnail_url}
+      </div>
+      <div>
+        likes: {likes}
+      </div>
+      <div>
+        recipeId: {recipeId}
+      </div>
+    </div>
+  )
+}
+
+export default RecipeBaseDetails
--- react-app/src/components/ViewEditRecipePage/ViewEditRecipePage.js
+++ react-app/src/components/ViewEditRecipePage/ViewEditRecipePage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './ViewEditRecipePage.css'
+import RecipeBaseDetails from '../Recipe/RecipeBaseDetails';
+import { useParams } from 'react-router';
+import { getRecipe } from '../../store/recipe';
+import { useDispatch } from 'react-redux';
+
+
 const ViewEditRecipePage = () => {
+
+  //the /:id from url, use the 'useParams' from react-router to get the correct id
+
+  let {id} = useParams();
+  const dispatch = useDispatch()
+
+  // getting the recipe based on ID and adding it to the store.
+  useEffect(()=>{
+    dispatch(getRecipe(id))
+  }, [dispatch, id])
+
+
   return (
     <div className="ver-page-container">
-      div check
       <div className="content-container">
-        <div className="recipe-base-container">recipe base</div>
+        <div className="recipe-base-container">
+          <RecipeBaseDetails/>
+        </div>
         <div className="recipe-photos-container">recipe photos</div>
         <div className="recipe-ingredients-container">ingredients</div>
         <div className="recipe-directions-container">directions</div>
