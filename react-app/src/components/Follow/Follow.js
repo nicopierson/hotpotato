a09a53438f164977ae1448e4fabf7687964cdacf
Nico Pierson <nicogpt@gmail.com>
2021-08-21 16:08:44 -0700

    Follow buttons update follow redux state

--- react-app/src/components/Follow/Follow.js
+++ react-app/src/components/Follow/Follow.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+
+import { removeFollowing, createFollowing } from '../../store/follow';
 
 import styles from './Follow.module.css';
 
 const Follow = () => {
     const { userId } = useParams();
+    const dispatch = useDispatch();
+
     const followers = useSelector(state => state.follow.followers);
     const followings = useSelector(state => state.follow.followings);
 
@@ -17,15 +21,27 @@ const Follow = () => {
     console.log(isFollowing);
     console.log('show follow: ', showFollow);
 
+    const handleAdd = (e) => {
+        e.preventDefault();
+        dispatch(createFollowing(userId))
+    };
+
+    const handleRemove = (e) => {
+        e.preventDefault();
+        dispatch(removeFollowing(userId));
+    };
+
     return (
         <div className={styles.follow_container}>
             <button
+                onClick={handleAdd}
                 className={styles.button_follow}
             >
                 <i className='fas fa-plus-circle'></i>
                 Follow
             </button>
             <button
+                onClick={handleRemove}
                 className={styles.button_followed}
             >
                 <span
