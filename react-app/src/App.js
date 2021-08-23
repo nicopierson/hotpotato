3aceb26d704eded52b4d19348a9f8f7083429243
wes <trinhpt@uci.edu>
2021-08-23 00:14:22 -0700

    updated nav

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -34,44 +34,46 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <Route path='/view/recipe/:recipeId' exact={true}>
-          <ViewEditRecipePage />
-        </Route>
-        <Route path='/card' exact={true}>
-          <RecipeCardComponent />
-        </Route>
-        <Route path='/explore' exact={true}>
-          <LandingPage />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
+      <div className = "container-app">
+        <Switch >
+          <Route path='/login' exact={true}>
+            <LoginForm />
+          </Route>
+          <Route path='/sign-up' exact={true}>
+            <SignUpForm />
+          </Route>
+          <Route path='/view/recipe/:recipeId' exact={true}>
+            <ViewEditRecipePage />
+          </Route>
+          <Route path='/card' exact={true}>
+            <RecipeCardComponent />
+          </Route>
+          <Route path='/explore' exact={true}>
+            <LandingPage />
+          </Route>
+          <ProtectedRoute path='/users' exact={true} >
+            <UsersList/>
+          </ProtectedRoute>
+          <ProtectedRoute path='/users/:userId' exact={true} >
+            <User />
+          </ProtectedRoute>
 
-        <ProtectedRoute path='/profile/:userId' exact={true} >
-          <ProfilePage />
-        </ProtectedRoute>
-        <ProtectedRoute path='/feed' exact={true} >
-          <FeedPage />
-        </ProtectedRoute>
+          <ProtectedRoute path='/profile/:userId' exact={true} >
+            <ProfilePage />
+          </ProtectedRoute>
+          <ProtectedRoute path='/feed' exact={true} >
+            <FeedPage />
+          </ProtectedRoute>
 
-        <ProtectedRoute path='/' exact={true} >
-            <Redirect to="/explore" />
-        </ProtectedRoute>
-        <Route path='/profile/:userId' exact={true}>
-          <ProfilePage />
-        </Route>
+          <ProtectedRoute path='/' exact={true} >
+              <Redirect to="/explore" />
+          </ProtectedRoute>
+          <Route path='/profile/:userId' exact={true}>
+            <ProfilePage />
+          </Route>
+        </Switch>
+      </div>
 
-      </Switch>
       <Footer />
 
     </BrowserRouter>
